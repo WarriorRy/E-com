de638dfa1afe10fec58b7ca64f42a533fcfaaa99
WarriorRy <2005riteshyadav1@gmail.com>
2024-12-11 22:08:09 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#2563EB',     // A brighter blue for primary elements, good for call-to-action buttons.
+        highlight: '#EFF6FF',   // Light blue highlight for soft accents, enhancing the white background.
+        bgGray: '#F3F4F6',      // Soft gray for subtle backgrounds or sections, creating depth without overwhelming the design.
+        danger: '#EF4444',      // Red for critical actions like deleting or canceling, attention-grabbing but not harsh.
+        secondaryGray: '#9CA3AF', // Muted gray for less prominent buttons or secondary actions.
       },
     },
   },
   plugins: [],
-};
+}
